17bb7448fc97acd22a53fca00c38e89c71456131
liuqi15 <liuqi15@baidu.com>
2019-04-18 05:53:47 -0700

    add java

--- readme/java/05/ConstructorDemo1.java
+++ readme/java/05/ConstructorDemo1.java
@@ -1,12 +1,31 @@
 class ConstructorDemo1 {
     public static void main(String[] args) {
         Dog d = new Dog();
+        d.watch();
+        Dog d1 = new Dog();
     }
 }
 
 class Dog {
-    private String name;
+    // 构造代码块
+    {
+        //System.out.println("1===>开始创建Dog()!!" + name);
+        name = "旺财";
+        //System.out.println("1===>开始创建Dog()!!" + name);
+    }
+    // 构造代码块
+    {
+        //System.out.println("2===>开始创建Dog()!!" + name);
+        name = "小强";
+        //System.out.println("1===>开始创建Dog()!!" + name);
+    }
+
+    private String name = "无名";
     private String color;
+
+    public Dog() {
+        System.out.println("new Dog()" + name);
+    }
     // 
     public void watch() {
         System.out.println("汪汪~~~~~");

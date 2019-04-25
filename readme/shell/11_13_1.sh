d89a63e2893fbe921da1c5889a5c709f9b7b8ae2
liuqi15 <liuqi15@baidu.com>
2019-04-25 02:25:34 -0700

    add 11_13_1.sh

--- readme/shell/11_13_1.sh
+++ readme/shell/11_13_1.sh
@@ -18,12 +18,36 @@ function CheckUrl() {
     do
         wget -T 5 --tries=1 --spider $1 >/dev/null 2>&1        
         if [ $? -ne 0 ];then
-            continue
-        else
             ((FAILCOUNT=FAILCOUNT+1))
+        else
+             break
         fi
     done
     echo $FAILCOUNT
+    return $FAILCOUNT
+}
+
+function Mail() {
+    local SUBJECT_CONTENT=$1
+    for MAIL_USER in `echo MAIL_GROUP`
+    do
+        mail -s $SUBJECT_CONTENT $MAIL_USER <$LOG_FILE 
+    done
+}
+
+function SendMsg() {
+    echo "param:"$1
+    if [ $1 -ge 3 ];then
+        RETVAL=1
+        NOWTIME=`date +"%Y-%m-%d %H:%M:%S"`
+        SUBJECT_CONTENT="http://$1 is error,${NOWTIME}."
+        echo ${NOWTIME} | tee $LOG_FILE
+        Mail $SUBJECT_CONTENT
+    else
+        echo "http://$1 is ok."
+        RETVAL=0
+    fi
+    return $RETVAL
 }
 
 function main() {
@@ -31,6 +55,7 @@ function main() {
     for url in `cat $path/domain.list`
     do
         CheckUrl $url
+        SendMsg $? $url
     done
 }
 

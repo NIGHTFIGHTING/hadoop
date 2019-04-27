c5aceb5fd0c6d55a259cb4d13f194a3935a584e1
liuqi15 <liuqi15@baidu.com>
2019-04-27 08:19:06 -0700

    add limit

--- readme/hadoop/TestHDFS-Recursive.java
+++ readme/hadoop/TestHDFS-Recursive.java
@@ -3,15 +3,28 @@ package com.it18zhang.myhadoop_maven;
 import org.junit.Test;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.io.IOUtils;
+import java.io.FileInputStream;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 
 /**
  * 测试hdfs
- **/
+ */
 public class TestHDFS 
 {
+	@Test
+    public void putFile() throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("fs.defaultFS", "hdfs://192.168.244.131:8020/");
+    	FileSystem fs = FileSystem.get(conf);
+    	FSDataOutputStream out = fs.create(new Path("/user/centos/hadoop/a.txt"), true, 1024, (short)2, 1024);
+    	IOUtils.copyBytes(new FileInputStream("C:\\Users\\liuqi15\\Desktop\\home.txt"), out, conf);
+        System.out.println("over");
+	}
+	
 	@Test
     public void listFS() throws Exception {
 		Configuration conf = new Configuration();
@@ -21,7 +34,7 @@ public class TestHDFS
     }
 	
 	/**
-	 * 递归输出hdfs目录结构
+	 *  递归输出hdfs目录结构
 	 **/
 	private void printlnPath(FileSystem fs, Path p) {
 		try {

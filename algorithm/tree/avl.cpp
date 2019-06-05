6ac786eea1b7178739629f2d9cefe177150f32ff
liuqi15 <liuqi15@baidu.com>
2019-06-04 20:21:38 -0700

    add tree

--- algorithm/tree/avl.cpp
+++ algorithm/tree/avl.cpp
@@ -16,6 +16,8 @@ struct TreeNode {
     }
 };
 
+TreeNode* right_rotate(TreeNode*& root);
+TreeNode* left_rotate(TreeNode*& root);
 void insert_tree(TreeNode** tree, int value) {
     if (tree == nullptr) {
         return ;
@@ -68,6 +70,7 @@ TreeNode* insert_tree2(TreeNode** tree, int value) {
     if ((*tree) == nullptr) {
         (*tree) = new TreeNode;
         (*tree)->value = value;
+        return (*tree);
     }
     if (value > (*tree)->value) {
         (*tree)->right = insert_tree2(&((*tree)->right), value);
@@ -81,7 +84,74 @@ TreeNode* insert_tree2(TreeNode** tree, int value) {
     if (abs(balance_factor) > 1) {
         std::cout << "unbalanced: " << balance_factor << std::endl;
     }
+
+    // 平衡维护
+    // LL
+    if (balance_factor > 1 && get_balance_factor((*tree)->left) >= 0) {
+        return right_rotate(*tree);
+    }
+    // RR
+    if (balance_factor < -1 && get_balance_factor((*tree)->right) <= 0) {
+        return left_rotate(*tree);
+    }
+    // LR
+    if (balance_factor > 1 && get_balance_factor((*tree)->left) < 0) {
+        (*tree)->left = left_rotate((*tree)->left);
+        return right_rotate(*tree);
+    }
+    // RL
+    if (balance_factor < -1 && get_balance_factor((*tree)->right) > 0) {
+        (*tree)->right = right_rotate((*tree)->right);
+        return left_rotate(*tree);
+    }
+
     return (*tree);
+}
+
+// 对节点y进行向右旋转操作，返回旋转后新的根节点x
+//        y                              x
+//       / \                           /   \
+//      x   T4     向右旋转 (y)        z     y
+//     / \       - - - - - - - ->    / \   / \
+//    z   T3                       T1  T2 T3 T4
+//   / \
+// T1   T2
+TreeNode* right_rotate(TreeNode*& y) {
+    TreeNode* x = y->left;
+    TreeNode* t3 = x->right;
+
+    // 向右旋转过程
+    x->right = y;
+    y->left = t3;
+
+    // 更新height
+    y->height = max(get_height(y->left), get_height(y->right)) + 1;
+    x->height = max(get_height(x->left), get_height(x->right)) + 1;
+
+    return x;
+}
+
+// 对节点y进行向左旋转操作，返回旋转后新的根节点x
+//    y                             x
+//  /  \                          /   \
+// T1   x      向左旋转 (y)       y     z
+//     / \   - - - - - - - ->   / \   / \
+//   t2  z                     T1 t2 t3 T4
+//      / \
+//     t3 T4
+TreeNode* left_rotate(TreeNode*& y) {
+    TreeNode* x = y->right;
+    TreeNode* t2 = x->left;
+
+    // 向左旋转过程
+    x->left = y;
+    y->right = t2;
+
+    // 更新height
+    y->height = max(get_height(y->left), get_height(y->right)) + 1;
+    x->height = max(get_height(x->left), get_height(x->right)) + 1;
+
+    return x;
 }
 
 TreeNode* create_tree(const std::vector<int> arr) {
@@ -152,6 +222,7 @@ bool is_balanced(TreeNode* root) {
     return get_balance_factor(root->left) && get_balance_factor(root->right); 
 }
 
+#if 0
 // 对接点y进行向左旋转操作,返回旋转后新的根节点x
 //     y                        x
 //    / \                      / \
@@ -161,7 +232,7 @@ bool is_balanced(TreeNode* root) {
 //        / \
 //       T3 T4
 
-void left_rorate(TreeNode** root) {
+TreeNode* left_rotate(TreeNode** root) {
     TreeNode* x = (*root)->right;
     TreeNode* t2 = x->left;
 
@@ -172,6 +243,7 @@ void left_rorate(TreeNode** root) {
     // 更新height
     (*root)->height = max(get_height((*root)->left), get_height((*root)->right)) + 1;
     x->height = max(get_height(x->left), get_height(x->right)) + 1;
+    return x;
 }
 
 
@@ -183,19 +255,21 @@ void left_rorate(TreeNode** root) {
 // z  T3                    T1 T2 T3 T4
 /// \
 //T1 T2
-void right_rorate(TreeNode** root) {
+TreeNode* right_rotate(TreeNode** root) {
     TreeNode* x = (*root)->left;
-    TreeNode* T3 = x->right;
+    TreeNode* t3 = x->right;
 
     // 向右旋转过程
     x->right = *root; 
-    (*root)->left = T3;
+    (*root)->left = t3;
 
     // 更新height
     (*root)->height = max(get_height((*root)->left), get_height((*root)->right)) + 1;
     x->height = max(get_height(x->left), get_height(x->right)) + 1;
+    return x;
 }
 
+#endif
 int main() {
     vector<int> arr{3,2,5,6,7,8,1,0,4,9};
     TreeNode* tree = create_tree(arr);

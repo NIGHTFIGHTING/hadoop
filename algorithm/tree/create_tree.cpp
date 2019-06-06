7f5d50fca94f1b969b3e88941d0e26fff5c75b2f
liuqi15 <liuqi15@baidu.com>
2019-06-06 16:03:54 -0700

    add kuangxiang

---
+++ algorithm/tree/create_tree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    std::string value;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+
+TreeNode* create_tree() {
+    std::string str;
+    cin >> str;
+    TreeNode* root = nullptr;
+    if (str != "#") {
+        root = new TreeNode;
+        root->value = str;
+        root->left = create_tree();
+        root->right = create_tree();
+    }
+    return root;
+}
+
+void pre_order(TreeNode* root) {
+    if (root == nullptr) {
+        return ; 
+    }
+    std::cout << root->value << " ";
+    pre_order(root->left);
+    pre_order(root->right);
+}
+
+int main() {
+    TreeNode* root = create_tree();
+    pre_order(root);
+    return 0;
+}

0e2ad00088435f3878527e0a2f0f7dbfd87d7a85
liuqi15 <liuqi15@baidu.com>
2019-06-07 16:54:50 -0700

    modify kuangxiang

--- nginx/kuangxiang/06/06.cpp
+++ nginx/kuangxiang/06/06.cpp
@@ -2,13 +2,15 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
 
 int main(int argc, char *const *argv)
 {             
+    //signal(SIGHUP,SIG_IGN); //SIG_IGN标志：我要求忽略这个信号，请操作系统不要用缺省的处理方式来对待我（不要把我杀掉）；
     printf("非常高兴，大家和老师一起学习《linux c++通讯架构实战》\n");
     for(;;)
     {
-        // sleep(1); //休息1秒
+         sleep(1); //休息1秒
         printf("休息1秒\n");
     }
     printf("程序退出，再见!\n");
